aebad3b1de285a780852111557089c68d5c73579
dinesh <dinesh.m477@gmail.com>
2023-09-11 15:36:26 +0530

    Commiting of ROR and Solvency

--- src/main/java/com/srit/pages/RORPage.java
+++ src/main/java/com/srit/pages/RORPage.java
@@ -281,5 +281,232 @@ public class RORPage extends BasePage{
     }
 
 
+    /*xpath for DA */
+
+    private final By lk_da_firstrecord=By.xpath("//mat-row[1]//mat-cell[2]//span");
+
+    private final By btn_manual_upload=By.xpath("//*[@ng-reflect-value='true' and contains(@id, 'mat-radio-')]");
+
+    private final By ror_file=By.xpath("//*[@class='col-xl-3 col-lg-3 col-md-3 col-sm-3 fieldname-half ng-star-inserted']//input");
+    private final By rc_update=By.xpath("//*[contains(text(),'Update')]");
+
+    private final By txt_searchingfee=By.xpath("//*[@formcontrolname='searchingFee']");
+
+    private final By txt_extrafee=By.xpath("//*[@ng-reflect-name='extraFee']");
+
+    private final By txt_noofpaper=By.xpath("//*[@ng-reflect-name='noOfPaper']");
+
+    private final By txt_others=By.xpath("//*[@ng-reflect-name='others']");
+
+    private final By txt_plainpapers=By.xpath("//*[@ng-reflect-name='plainPapers']");
+
+    private final By txt_printedpapers=By.xpath("//*[@ng-reflect-name='printedPaper']");
+
+    private final By txt_tracingcloth=By.xpath("//*[@ng-reflect-name='tracingCloth']");
+
+    private final By txt_copyingfee=By.xpath("//*[@ng-reflect-name='copyingFee']");
+
+    private final By txt_noofwords=By.xpath("//*[@ng-reflect-name='noOfWords']");
+
+    private final By btn_verification_submit=By.xpath("//mat-dialog-actions//button//span[text()='Submit']");
+    private final By btn_verification_continue=By.xpath("//span[@class='mdc-button__label' and text()='Continue']");
+
+    private final By txt_da_remarks=By.xpath("//*[@ng-reflect-name='remarks']");
+    private final By btn_action_drpdwn=By.xpath("//*[@ng-reflect-name='action']");
+    private final By lst_action_drpdwn=By.xpath("//*[@class='mdc-list-item__primary-text']");
+
+    private final By btn_office_drpdwn=By.xpath("//*[@ng-reflect-name='office']");
+
+    private final By lst_office_drpdwn=By.xpath("//*[@class='mdc-list-item__primary-text']");
+
+    private final By btn_tahsil_forward=By.xpath("//td[contains(text(),'Approving')]//following-sibling::td//mat-radio-button//div//div//input");
+
+    private final By btn_ri_forward=By.xpath("//td[contains(text(),'Processing')]//following-sibling::td//mat-radio-button//div//div//input");
+
+    private final By btn_da_submit=By.xpath("//span[@class='mdc-button__label' and text()='Submit']");
+
+    private final By btn_da_continue=By.xpath("//span[@class='mdc-button__label' and text()='Continue']");
+
+    private final By btn_da_close=By.xpath("//span[@class='mdc-button__label' and text()='Close']");
+
+    public void txt_da_remarks(String value)
+    {
+        sendkeys(txt_da_remarks, value, WaitStrategy.PRESENCE);
+    }
+    public void click_firstrecord()
+    {
+        click(lk_da_firstrecord, WaitStrategy.CLICKABLE);
+    }
+
+    public WebElement click_manualUpload()
+    {
+        return getWebElement(btn_manual_upload);
+    }
+
+    public WebElement rorfileUpload()
+    {
+        return getWebElement(ror_file);
+    }
+
+    public WebElement getUpdateVerification()
+    {
+       return getWebElement(rc_update);
+    }
+
+
+    public void txt_searchingfee(String value)
+    {
+        sendkeys(txt_searchingfee, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_extrafee(String value)
+    {
+        sendkeys(txt_extrafee, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_noofpaper(String value)
+    {
+        sendkeys(txt_noofpaper, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_others(String value)
+    {
+        sendkeys(txt_others, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_plainpers(String value)
+    {
+        sendkeys(txt_plainpapers, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_printedpapers(String value)
+    {
+        sendkeys(txt_printedpapers, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_tracingcloth(String value)
+    {
+        sendkeys(txt_tracingcloth, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_copyingfee(String value)
+    {
+        sendkeys(txt_copyingfee, value, WaitStrategy.PRESENCE);
+    }
+
+    public void txt_noofwords(String value)
+    {
+        sendkeys(txt_noofwords, value, WaitStrategy.PRESENCE);
+    }
+    public void clickVerificationSubmit()
+    {
+        click(btn_verification_submit, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickVerificationContinue()
+    {
+        click(btn_verification_continue, WaitStrategy.CLICKABLE);
+    }
+    public WebElement actiondropdown()
+    {
+        return getWebElement(btn_action_drpdwn);
+    }
+
+    public By actiondropdownlist()
+    {
+        return lst_action_drpdwn;
+    }
+    public WebElement officedropdown()
+    {
+        return getWebElement(btn_office_drpdwn);
+    }
+
+    public By officedropdownlist()
+    {
+        return lst_office_drpdwn;
+    }
+
+    public WebElement forward()
+    {
+        return getWebElement(btn_tahsil_forward);
+    }
+
+    public WebElement forwardtori()
+    {
+        return getWebElement(btn_ri_forward);
+    }
+    public void click_forward()
+    {
+        click(btn_tahsil_forward, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickdasubmit()
+    {
+        click(btn_da_submit, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickdacontinue()
+    {
+        click(btn_da_continue, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickdaclose()
+    {
+        click(btn_da_close, WaitStrategy.CLICKABLE);
+    }
+
+    /* xpath for Head Clerk */
+
+    private final By txt_searchbox=By.xpath("//input[@placeholder='Search']");
+
+    private final By lk_th_firstrecord=By.xpath("//mat-row[1]//mat-cell[2]//span//a");
+
+    private final By btn_applydsc=By.xpath("//span[@class='mdc-button__label' and text()='Apply Digital Signature']");
+
+
+    public void txt_hc_remarks(String value)
+    {
+        sendkeys(txt_da_remarks, value, WaitStrategy.PRESENCE);
+    }
+
+    public WebElement tahsilfirstrecord()
+    {
+        return getWebElement(lk_th_firstrecord);
+    }
+    public void txt_searchbox(String value)
+    {
+        sendkeys(txt_searchbox, value, WaitStrategy.PRESENCE);
+    }
+
+    public void clickhcsubmit()
+    {
+        click(btn_da_submit, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickhccontinue()
+    {
+        click(btn_da_continue, WaitStrategy.CLICKABLE);
+    }
+
+    /*xpath for Tahsil */
+
+
+    public void clickTahsilsubmit()
+    {
+        click(btn_da_submit, WaitStrategy.CLICKABLE);
+    }
+
+    public void clickTahsilcontinue()
+    {
+        click(btn_da_continue, WaitStrategy.CLICKABLE);
+    }
+    public void txt_tahsil_remarks(String value)
+    {
+        sendkeys(txt_da_remarks, value, WaitStrategy.PRESENCE);
+    }
+    public void clickDigitalSignature()
+    {
+        click(btn_applydsc, WaitStrategy.CLICKABLE);
+    }
 
 }
--- src/test/java/com/srit/tests/RorCertificate.java
+++ src/test/java/com/srit/tests/RorCertificate.java
@@ -3,16 +3,19 @@ package com.srit.tests;
 import com.srit.config.DropDownUtils;
 import com.srit.config.FileUploadUtils;
 import com.srit.config.HandlePop;
+import com.srit.config.JavaScriptUtils;
 import com.srit.constants.FrameworkConstants;
+import com.srit.driver.DriverManager;
 import com.srit.pages.CoreApplicationPage;
 import com.srit.pages.RORPage;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RorCertificate extends BaseTest{
+public class RorCertificate extends BaseTest
+{
 
 String ror=null;
-
 @Test
     public void applyforRorService()
 {
@@ -108,12 +111,150 @@ String ror=null;
             throw new RuntimeException(e);
         }
 
+    } catch (Exception e)
+    {
+        throw new RuntimeException(e);
+    }
+
+
+
+
+}
+
+    @Test(dependsOnMethods = "applyforRorService")
+    public void forwardbyRPtoHC()
+    {
+
+        try
+        {
+            CoreApplicationPage cp= new CoreApplicationPage();
+            cp.enter_username("rec.barg").enter_password("Pass@1231")
+                    .clickLogin()
+                    .clickCoreApplication().
+                    click_applicationbox().
+                    click_mypendingapplications().click_rorapplications();
+            RORPage rr= new RORPage();
+            rr.click_firstrecord();
+            Thread.sleep(2000);
+            DropDownUtils.dropdownSelect(rr.click_manualUpload());
+            FileUploadUtils.fileUpload(rr.rorfileUpload(), FrameworkConstants.getUploadfilepath());
+            Thread.sleep(1000);
+            DropDownUtils.dropdownSelect(rr.getUpdateVerification());
+            HandlePop.getHandlePop();
+            rr.txt_searchingfee("10");
+            rr.txt_extrafee("10");
+            rr.txt_noofpaper("10");
+            rr.txt_others("10");
+            rr.txt_plainpers("10");
+            rr.txt_printedpapers("10");
+            rr.txt_tracingcloth("10");
+            rr.txt_copyingfee("10");
+            rr.txt_noofwords("10");
+            rr.clickVerificationSubmit();
+            rr.clickVerificationContinue();
+            HandlePop.getHandlePop();
+            rr.txt_da_remarks("some remarks");
+            Thread.sleep(1000);
+            DropDownUtils.dropdownSelect(rr.actiondropdown());
+            Thread.sleep(1000);
+            DropDownUtils.dropdownList(rr.actiondropdownlist(), "forward");
+            Thread.sleep(2000);
+            DropDownUtils.dropdownSelect(rr.officedropdown());
+            Thread.sleep(1000);
+            DropDownUtils.dropdownList(rr.officedropdownlist(), "tahsil Office");
+            Thread.sleep(1000);
+            JavaScriptUtils.ScrollIntoView(rr.forwardtori());
+            rr.clickdasubmit();
+            HandlePop.getHandlePop();
+            rr.clickdacontinue();
+            HandlePop.getHandlePop();
+            rr.clickdaclose();
+            rr.clickUser();
+            rr.clickLogout();
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Test(dependsOnMethods = "forwardbyRPtoHC")
+    public void forwardbyRPtoTahsil()
+    {
+        try
+        {
+            CoreApplicationPage cp= new CoreApplicationPage();
+            cp.enter_username("hc.bargarh").enter_password("Pass@1231")
+                    .clickLogin()
+                    .clickCoreApplication().
+                    click_applicationbox().
+                    click_mypendingapplications().click_rorapplications();
+            RORPage rr= new RORPage();
+            rr.txt_searchbox(ror);
+            Thread.sleep(2000);
+            DropDownUtils.dropdownSelect(rr.tahsilfirstrecord());
+            rr.txt_hc_remarks("some remarks");
+            Thread.sleep(1000);
+            DropDownUtils.dropdownSelect(rr.actiondropdown());
+            Thread.sleep(1000);
+            DropDownUtils.dropdownList(rr.actiondropdownlist(), "forward");
+            Thread.sleep(1000);
+            JavaScriptUtils.ScrollIntoView(rr.officedropdown());
+            DropDownUtils.dropdownSelect(rr.officedropdown());
+            Thread.sleep(1000);
+            DropDownUtils.dropdownList(rr.officedropdownlist(), "tahsil Office");
+            Thread.sleep(1000);
+            JavaScriptUtils.ScrollIntoView(rr.forward());
+            rr.clickhcsubmit();
+            HandlePop.getHandlePop();
+            rr.clickhccontinue();
+            String confirm= rr.getConfirmText().substring(12,26);
+            System.out.println("the Application No:" +confirm);
+            Assert.assertEquals(ror, confirm, "tribe No didnt match");
+            rr.clickdaclose();
+            rr.clickUser();
+            rr.clickLogout();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+@Test(dependsOnMethods = "forwardbyRPtoTahsil")
+    public void approveByTahsil()
+{
+    try
+    {
+        CoreApplicationPage cp= new CoreApplicationPage();
+        cp.enter_username("tdr.bargarh").enter_password("Pass@1231")
+                .clickLogin()
+                .clickCoreApplication().
+                click_applicationbox().
+                click_mypendingapplications().click_rorapplications();
+        RORPage rr= new RORPage();
+        rr.txt_searchbox(ror);
+        Thread.sleep(2000);
+        DropDownUtils.dropdownSelect(rr.tahsilfirstrecord());
+        JavascriptExecutor js=  (JavascriptExecutor) DriverManager.getDriver();
+        js.executeScript("window.scrollBy(0,1000)", "");
+        rr.txt_tahsil_remarks("some remarks");
+        Thread.sleep(1000);
+        DropDownUtils.dropdownSelect(rr.actiondropdown());
+        Thread.sleep(1000);
+        DropDownUtils.dropdownList(rr.actiondropdownlist(), "approve");
+        rr.clickTahsilsubmit();
+        HandlePop.getHandlePop();
+        rr.clickTahsilcontinue();
+        HandlePop.getHandlePop();
+        rr.clickDigitalSignature();
+
+
     } catch (Exception e) {
         throw new RuntimeException(e);
     }
 }
-
-
-
 
 }
